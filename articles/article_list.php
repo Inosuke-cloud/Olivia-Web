61748fc7660dad15fcceb516bad5e58781816bc7
daffaz <67418594+daffaz@users.noreply.github.com>
2020-10-08 18:34:06 +0700

    Add files via upload

---
+++ articles/article_list.php
@@ -0,0 +1,266 @@
+<!-- INI UNTUK TABLET KEATAS -->
+<!-- <section id="header-desktop" class="d-none d-sm-block"> -->
+<section id="header-desktop">
+    <div class="container mx-auto">
+        <div class="row pt-3 pb-3 mx-auto" style="width: 95%;">
+            <div class="col">
+                <div class="box1 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box-2 mx-auto">
+                <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ntar aja lah -->
+            <div class="col">
+                <div class="box-3 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box1 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box-2 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box-3 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row pb-3 mx-auto" style="width: 95%;">
+            <div class="col">
+                <div class="box1 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box-2 mx-auto">
+                <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box-3 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box1 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box-2 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box-3 mx-auto">
+                    <div class="row p-3">
+                        <div class="col text-white text-right">
+                            <span style="font-size: 0.8em;">24/05/2020</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white font-weight-bolder">
+                            <span style="font-size: 2.5em;">All is <br>Good</span>
+                        </div>
+                    </div>
+
+                    <div class="row p-3">
+                        <div class="col text-white">
+                            <span style="font-size: 0.8em;">Author Name Here</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- INI UNTUK TABLET KEBAWAH -->
+<!-- Menggunakan max width pada style2.css -->
